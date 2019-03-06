5e54c370b92bfc24426af85000b9c45508d514de
Oleg Port <oleg.portnikhin@eteron.ru>
2019-03-06 19:03:11 +0300

    Last date function.

--- network/twitter.py
+++ network/twitter.py
@@ -8,7 +8,12 @@ api = twitter.Api(consumer_key=CONSUMER_KEY,
                   access_token_secret=ACCESS_TOKEN_SECRET)
 
 
-def get_twitter_urls(slug, owner_screen_name):
+def get_twitter_urls_data(slug, owner_screen_name):
     twitter_data = api.GetListTimeline(slug=slug, owner_screen_name=owner_screen_name, count=200)
-    twitter_urls = [url.expanded_url for data in twitter_data for url in data.urls if 'twitter.com' not in url.expanded_url]
-    return twitter_urls
+    twitter_urls_data = []
+    for data in twitter_data:
+        for url in data.urls:
+            if 'twitter.com' not in url.expanded_url:
+                twitter_urls_data.append({'url': url.expanded_url, 'date': ''})
+    #twitter_urls_data = [{'url': url.expanded_url, 'date': ''} for data in twitter_data for url in data.urls if 'twitter.com' not in url.expanded_url]
+    return twitter_urls_data
--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 from tqdm import tqdm
 
 from network.internet import get_response
-from network.twitter import get_twitter_urls
+from network.twitter import get_twitter_urls_data
 from repo.url_data import save_to_csv
 from usecase.manager import get_url_data, get_lists_data
 
@@ -9,7 +9,7 @@ twitter_lists = get_lists_data()
 url_data_list = []
 
 for twitter_list in twitter_lists:
-    twitter_urls = get_twitter_urls(
+    twitter_urls = get_twitter_urls_data(
         slug=twitter_list['slug'],
         owner_screen_name=twitter_list['owner_screen_name']
     )
